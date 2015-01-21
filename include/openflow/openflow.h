e85327134dac2ea4b87fddedf973f02a0e883892
Thomas Boros <tomas.boros92@gmail.com>
2015-01-21 23:59:07 +0100

    compile error vrr

--- include/openflow/openflow.h
+++ include/openflow/openflow.h
@@ -327,9 +327,9 @@ enum oxm_ofb_match_fields {
     OFPXMT_OFB_MPLS_BOS = 36,       /* MPLS BoS bit. */
     OFPXMT_OFB_PBB_ISID = 37,       /* PBB I-SID. */
     OFPXMT_OFB_TUNNEL_ID = 38,      /* Logical Port Metadata. */
-    OFPXMT_OFB_IPV6_EXTHDR = 39     /* IPv6 Extension Header pseudo-field */
+    OFPXMT_OFB_IPV6_EXTHDR = 39,     /* IPv6 Extension Header pseudo-field */
     OFPXMT_OFB_TCP_SEQ = 40,        /* TCP sequence number. */
-    OFPXMT_OFB_TCP_ACK = 41,        /* TCP acknowledge number. */
+    OFPXMT_OFB_TCP_ACK = 41        /* TCP acknowledge number. */
 };
 
 /* The VLAN id is 12-bits, so we can use the entire 16 bits to indicate
