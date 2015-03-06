dfe41da6cfdad2b404b1d138cccfbf95974480ea
Thomas Boros <tomas.boros92@gmail.com>
2015-03-06 19:21:08 +0100

    comparing two match fields using cookies...rest_router should count with this

--- udatapath/flow_table.c
+++ udatapath/flow_table.c
@@ -116,7 +116,7 @@ flow_table_add(struct flow_table *table, struct ofl_msg_flow_mod *mod, bool chec
         }
 
         /* if the entry equals, replace the old one */
-        if (flow_entry_matches(entry, mod, true/*strict*/, false/*check_cookie*/)) {
+        if (flow_entry_matches(entry, mod, true/*strict*/, true/*check_cookie*/)) {
             VLOG_WARN_RL(LOG_MODULE, &rl, "Switch says, the same flow already exits so is rewritten");
             new_entry = flow_entry_create(table->dp, table, mod);
             *match_kept = true;
